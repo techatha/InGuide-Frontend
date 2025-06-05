bf07415ff041bf15a5ba12d90d9d6f3bca77dd87
techatha <techatham_p@cmu.ac.th>
2025-06-05 17:56:25 +0700

    https config for ip 192.168.1.142

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import fs from 'fs';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync('./localhost+3-key.pem'),
+      cert: fs.readFileSync('./localhost+3.pem')
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
